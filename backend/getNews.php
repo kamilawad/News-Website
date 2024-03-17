8289b71e78d9dc16e673e83bdc211acfccd1835e
kamilawad1 <kamilawad15@gmail.com>
2024-03-17 22:23:27 +0200

    added connection to databse and get news api

---
+++ backend/getNews.php
@@ -0,0 +1,30 @@
+<?php
+include('connection.php');
+
+$query = $mysqli->prepare('select * from news');
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+
+if ($num_rows == 0) {
+    $response['status'] = 'no news';
+} else {
+    $query->bind_result($id, $title, $text, $author, $created_at, $updated_at);
+    $news = [];
+    while ($query->fetch()) {
+        $new =[
+            'id' => $id,
+            'title' => $title,
+            'text' => $text,
+            'author' => $author,
+            'created_at' => $created_at,
+            'updated_at' => $updated_at
+        ];
+    $news[] = $new;
+}
+}
+
+$response['status'] = "success";
+$response['news'] = $new;
+
+echo json_encode($response);
